ea6f6ef15dceca10b27733fe06a17148f84eb8a2
Chloe <waltonct5@gmail.com>
2017-09-08 08:48:42 -0700

    Initial commit; notes and examples

---
+++ For_loops.rb
@@ -0,0 +1,13 @@
+# Three dots. Don't Include Last
+# Two dots. Include Last
+
+for num in 1..20
+  puts num
+end
+
+
+=begin
+For the variable num in the range 1 to 10, do the following." 
+The following was to puts "#{num}", so as num took on the values of 1 to 9, 
+one at a time, those values were printed to the console.
+=end
