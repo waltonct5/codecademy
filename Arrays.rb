fc9f8b19d6496b3d2e1dbcecc017b28c82847b98
Chloe <waltonct5@gmail.com>
2017-09-08 17:29:35 -0700

    up to 2-day arrays

--- Arrays.rb
+++
@@ -1,5 +0,0 @@
-=begin
-Arrays are just storing multiple values in a VARIABLE to late LOOP AND ITERATE
-=end
-
-my_array = [1,2,3,4,5]
---
+++ Arrays_data_structures.rb
@@ -0,0 +1,15 @@
+=begin
+Arrays are just storing multiple values in a VARIABLE to late LOOP AND ITERATE
+
+remember: each element in the array has what's called an index; begins at 0
+
+We can access elements of the array directly through these numbers using brackets
+=end
+
+my_array = [1,4,3,7,5]
+array[4]
+# returns "5" since "5" is at index 4
+
+# An array (like a line) is one-dimensional; an array of arrays (like a square) is two-dimensional.
+
+my_2d_array = [[1, 1], [1, 1]]
